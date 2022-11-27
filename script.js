88b12fbcbd958bed9b63a2fff2347d9685f5b620
Pranay <pranaydwivedi32@gmail.com>
2022-11-27 19:12:03 +0530

    Solved Decimal Issue with JS
    Used Math.floor and to fixed methods

--- script.js
+++ script.js
@@ -5,11 +5,24 @@
 //Indian names and login
 //deleting account popup
 //logout feauture
+//add background color feautre
 const account1 = {
   owner: "Jonas Schmedtmann",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account2 = {
@@ -17,6 +30,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -24,6 +49,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account4 = {
@@ -31,6 +68,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -85,7 +134,7 @@ const displayMov = function (movements, sort = false) {
     let type = amount > 0 ? "deposit" : "withdrawal";
     let movHTML = `<div class="movements__row">
   <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-  <div class="movements__value">${amount}€</div>
+  <div class="movements__value">${amount.toFixed(2)}€</div>
 </div>`;
     containerMovements.insertAdjacentHTML("afterbegin", movHTML);
   });
@@ -122,7 +171,7 @@ const usernameGenerator = function (account) {
 //Calculating Balance function
 const calcDisplayBalance = function (acc) {
   //creating array of net balance
-  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0).toFixed(2);
   // console.log(balance);
   labelBalance.textContent = acc.balance;
   // return balance;
@@ -133,11 +182,11 @@ const calcDisplaySummary = function (movements) {
   const incomes = movements
     .filter((mov) => mov > 0)
     .reduce((sum, mov) => sum + mov, 0);
-  labelSumIn.textContent = incomes;
+  labelSumIn.textContent = incomes.toFixed(2);
   const out = Math.abs(
     movements.filter((mov) => mov < 0).reduce((sum, mov) => sum + mov, 0)
   );
-  labelSumOut.textContent = out;
+  labelSumOut.textContent = out.toFixed(2);
   const interest = movements
     .filter(function (mov) {
       return mov > 0;
@@ -145,7 +194,7 @@ const calcDisplaySummary = function (movements) {
     .map((deposits) => (deposits * 1.2) / 100)
     .filter((mov) => mov >= 1)
     .reduce((sum, interest) => sum + interest, 0);
-  labelSumInterest.textContent = interest;
+  labelSumInterest.textContent = interest.toFixed(2);
 };
 
 usernameGenerator(accounts);
@@ -222,7 +271,7 @@ btnTransfer.addEventListener("click", userTransfer);
 //Loan Function
 const loanRequest = function (e) {
   e.preventDefault();
-  let loanAmount = Number(inputLoanAmount.value);
+  let loanAmount = Math.floor(inputLoanAmount.value);
   if (
     loanAmount > 0 &&
     currentAccount.movements.some((mov) => mov >= loanAmount * 0.1)
